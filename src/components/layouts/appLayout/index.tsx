138fc22ddc86ba5c244d22f284add42998d54361
Jenish Shrestha <jenishhrestha@gmail.com>
2025-04-06 23:24:51 +0545

    Build Fix

--- src/components/layouts/appLayout/index.tsx
+++ src/components/layouts/appLayout/index.tsx
@@ -8,33 +8,33 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 
 const nunito = Nunito({
-	variable: "--font-nunito",
-	subsets: ["latin"],
+  variable: "--font-nunito",
+  subsets: ["latin"],
 });
 
 const ptSans = PT_Sans({
-	variable: "--font-pt-sans",
-	subsets: ["latin"],
-	weight: ["400", "700"],
+  variable: "--font-pt-sans",
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 const AppLayout: React.FC<{ children: React.ReactNode }> = (props) => {
-	const theme = useSelector((state: RootState) => state.themeToggle.mode);
+  const theme = useSelector((state: RootState) => state.themeToggle.mode);
 
-	const { children } = props;
-	return (
-		<html
-			lang="en"
-			className={`text-base ${theme ? theme : "light"}`}
-			data-theme={theme}
-		>
-			<body
-				className={`${nunito.variable} ${ptSans.variable} antialiased relative`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  const { children } = props;
+  return (
+    <html
+      lang="en"
+      className={`text-base ${theme ? theme : "light"}`}
+      data-theme={theme}
+    >
+      <body
+        className={`${nunito.variable} ${ptSans.variable} relative antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 };
 
 export default AppLayout;
